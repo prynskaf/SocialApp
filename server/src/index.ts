34583e6c9d262e1a7b865708e5934fe2cc646ce8
Prince kyei <prince.kaf12@gmail.com>
2024-05-09 02:05:10 +0200

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,6 +30,17 @@ app.use("/api", router);
 // Serve uploaded images
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// Set CORS headers
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://social-app-phi-eight.vercel.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
